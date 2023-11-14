5ef3e80949640dd0041ee2657e2bc9211d545d28
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-11-14 20:13:50 +0500

    all

---
+++ routes/form.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+    res.send(`  
+    <form action="/form/submit" method="POST">
+    <input name="data"/>
+    <button>Submit</button>
+    </form>
+`)
+})
+
+router.post("/submit" , (req, res) => {
+    res.send(req.body);
+})
+
+module.exports = router;
