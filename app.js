50b0dce3e7438554d4543976d141a40ece6bcaf8
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-11-14 20:28:21 +0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
 const express = require("express");
 const bodyParser = require("body-parser");
+const form = require("./routes/form");
+const path = require("path");
 const app = express();
 
 app.use(bodyParser.urlencoded({extended : false}))
-app.use(bodyParser.json())
+app.use(bodyParser.json());
+app.use(express.static(path.join(process.cwd(), "public")));
 
-const form = require("./routes/form");
 app.use((req, res, next) => {
     req.data = "MUDDUSSIR";
     next();
