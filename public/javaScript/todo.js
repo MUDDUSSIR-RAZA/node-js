4db889f9167270eeed6b798dcf244715dc1ae314
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-12-03 21:50:13 +0500

    Update todo.js

--- public/javaScript/todo.js
+++ public/javaScript/todo.js
@@ -1,51 +1,13 @@
-const {deleteTodo} = require("../../controllers/todo")
-const deleteButtons = document.querySelectorAll(".delete-button");
+const otherCheckbox = document.querySelector("#other");
 
+otherCheckbox.addEventListener("change", () => {
+  //const todoId = checkbox.getAttribute("data-id");
 
-deleteButtons.forEach(function (button) {
-  button.addEventListener("click", function () {
-    const todoId = button.getAttribute("data-todo-id");
-    alert(todoId);
+  console.log(otherCheckbox.checked);
 
-
-    fetch(`/delete?index=${todoId}`, {
-      method: "GET",
-    })
-      .then((response) => response.json())
-      .then((data) => {
-        console.log(data); // Log the server response
-        // Optionally, update the UI on successful deletion
-      })
-      .catch((error) => console.error("Error:", error));
-   });
-
-
-
-
-
-
-
-
-   336
+  // if (otherCheckbox.checked) {
+  //   console.log("change");
+  // } else {
+  //   console.log("not change");
+  // }
 });
-
-// document.addEventListener('DOMContentLoaded', function () {
-
-//     deleteButtons.forEach(function (button) {
-//         button.addEventListener('click', function () {
-//             const todoId = button.getAttribute('data-todo-id');
-//             const todoIndex = button.getAttribute('data-todo-index');
-
-//             // Send a request to the server to delete the todo
-//             fetch(`/delete?index=${todoIndex}`, {
-//                 method: 'GET',
-//             })
-//             .then(response => response.json())
-//             .then(data => {
-//                 console.log(data); // Log the server response
-//                 // Optionally, update the UI on successful deletion
-//             })
-//             .catch(error => console.error('Error:', error));
-//         });
-//     });
-// });
