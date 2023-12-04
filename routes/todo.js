0a1e316fe37e8964147da6e58c775a34a38ebc76
MUDDUSSIR AWAN <muddussir1247@gmail.com>
2023-12-04 06:59:15 +0500

    Update todo.js

--- routes/todo.js
+++ routes/todo.js
@@ -1,29 +1,44 @@
 const express = require("express");
 const router = express.Router();
-const {deleteTodo , addTodo, getAlltodos} = require("../controllers/todo");
+const {
+  deleteTodo,
+  addTodo,
+  getAlltodos,
+  checkUpdate,
+  todoUpdate,
+} = require("../controllers/todo");
 
-router.get("/" , (req ,res) => {
-    res.render("todo" , {todos : getAlltodos()});
-})
+router.get("/", (req, res) => {
+  res.render("todo", { todos: getAlltodos() });
+});
 
-router.post("/add" , (req ,res) => {
-    addTodo(req.body.data);
-    res.redirect("/todo");
-})
+router.get("/todoUpdate/:id", (req, res) => {
+  const id = req.params.id;
+  res.render("todoUpdate", { id });
+});
+
+router.post("/add", (req, res) => {
+  addTodo(req.body.data);
+  res.redirect("/todo");
+});
+
+router.post("/update/:id", (req, res) => {
+  const id = req.params.id;
+  const todo = req.body.data;
+  todoUpdate(id , todo);
+  res.redirect("/todo")
+});
 
 router.get("/delete/:id", (req, res) => {
-    const { id } = req.params;
-    deleteTodo(id);
-    res.redirect("/todo");
+  const { id } = req.params;
+  deleteTodo(id);
+  res.redirect("/todo");
 });
 
-router.get("/check/:id", (req, res) => {
-    const { id } = req.params;
-    const check = req.body.data;
-    
-   console.log("id : " + id + " ischeck : " + check);
+router.post("/check/:id", (req, res) => {
+  const { id } = req.params;
+  const check = req.body.done;
+  checkUpdate(id, check);
 });
-
-
 
 module.exports = router;
